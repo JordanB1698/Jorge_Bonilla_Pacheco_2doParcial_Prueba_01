bb466b1364b3476a7f996ac23a22b0366971c58d
JordanB1698 <jbonillapacheco@gmail.com>
2022-01-10 18:10:30 -0500

    Agregado

---
+++ src/main/java/com/example/jorge_bonilla_pacheco_2do_prueba_01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jorge_bonilla_pacheco_2do_prueba_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
